aac107f75a721174eeabe26094ce08077e73c9fc
sajjad1112ali <sajjadramzan1211@gmail.com>
2023-05-15 15:50:33 +0500

    Added view

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use(express.static('public'));
-
+const downloadImagesController = require("./controllers/downloadImages.controller")
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/downloadImages/index.html'));
+})
+app.post('/download-images', async (req, res) => {
+  const result = await downloadImagesController.downloadImages(req)
+  await downloadImagesController.zipFolder(result);
+  res.send({ downloaded: result });
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
---
+++ public/js/script.js
@@ -0,0 +1,35 @@
+$("#configurationForm").submit(function (event) {
+  event.preventDefault();
+  $("#submitBtn").attr("disabled", true)
+
+  let posting = {
+    imagesCount: $("#imagesCount").val(),
+    height: $("#height").val(),
+    width: $("#width").val(),
+  };
+
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  let raw = JSON.stringify(posting);
+
+  let requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+  fetch("http://127.0.0.1:4000/download-images", requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result)
+      const folderName = result.downloaded;
+      const dirName = folderName.substring(8, folderName.length);
+
+      $("#downloadImages").attr("href", `http://127.0.0.1:4000/${dirName}/package`);
+      $('#configurationForm').trigger("reset");
+      if (result.success) $("#successMessage").removeClass('d-none');
+      $("#submitBtn").attr("disabled", false)
+    })
+    .catch((error) => console.log("error", error));
+});
